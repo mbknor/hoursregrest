43f63d96b69ac0d55d5eb93c9bea6c68a2673a90
Morten Kjetland <mbk@kjetland.com>
2010-02-25 21:18:03 +0100

    more work on the selectdateactiontest - not complete yet

--- src/test/scala/com/kjetland/hoursregrest/client/actions/SelectDateActionTest.scala
+++ src/test/scala/com/kjetland/hoursregrest/client/actions/SelectDateActionTest.scala
@@ -1,10 +1,13 @@
 package com.kjetland.hoursregrest.client.actions
 
-import org.easymock.EasyMock
-import com.kjetland.hoursregrest.client.Client
+
+import org.easymock.EasyMock.expect
+import org.easymock.classextension.EasyMock._
 import org.junit.{Test}
 import com.kjetland.hoursregrest.client.model.SelectedDate
 import org.joda.time.{DateMidnight}
+import com.kjetland.hoursregrest.client.parser.DayLinkResolver
+import com.kjetland.hoursregrest.client.{Browser, FormElement, Client}
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,28 +18,58 @@ import org.joda.time.{DateMidnight}
  */
 
 @Test
-class SelectDateActionTest{
-
+class SelectDateActionTest {
   @Test
-  def test_isOnCorrectDate{
+  def test_isOnCorrectDate {
 
     val date = new DateMidnight
 
-    val client = EasyMock.createMock( classOf[Client])
-    EasyMock.expect(client.selectedDate).andReturn( new SelectedDate(date) )
-    EasyMock.replay( client )
+    val client = createMock(classOf[Client])
+    expect(client.selectedDate).andReturn(new SelectedDate(date))
+    replay(client)
 
-    val a = new SelectDateAction( client, null )
+    val a = new SelectDateAction(client, null)
 
-    a.selectDate( date )
+    a.selectDate(date)
 
-    EasyMock.verify( client )
+    verify(client)
 
     return
   }
 
   @Test
-  def test_differentDaySameMonth{
+  def test_correctMonthButNoDayIsSelected {
+    val date = new DateMidnight
+
+    val client = createMock(classOf[Client])
+    //return this year and month but no selected day
+    expect(client.selectedDate).andReturn(new SelectedDate(date.year.get, date.monthOfYear.get))
+
+    val originalFormElements = List(new FormElement("name", "value"))
+
+    expect( client.formElements ).andReturn( originalFormElements )
     
+
+    val dayLinkId = "1234"
+
+    val dayLinkResolver = createMock( classOf[DayLinkResolver])
+    
+    expect(dayLinkResolver.dayLink( date.dayOfMonth.get )).andReturn(dayLinkId)
+
+    val browser = createMock( classOf[Browser])
+
+    val url = "myUrl"
+    expect( browser.url).andReturn( url )
+
+
+    expect( client.browser ).andReturn( browser)
+
+    replay(dayLinkResolver, client, browser)
+
+    val a = new SelectDateAction(client, dayLinkResolver)
+
+    a.selectDate( date )
+
+    verify(client, dayLinkResolver)
   }
 }
