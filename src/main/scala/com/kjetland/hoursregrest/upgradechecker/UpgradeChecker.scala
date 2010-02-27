01482547eef823e1501f9fdf27a42f39ec23cf76
Morten Kjetland <mbk@kjetland.com>
2010-02-27 22:27:30 +0100

    started working on upgrade manager

---
+++ src/main/scala/com/kjetland/hoursregrest/upgradechecker/UpgradeChecker.scala
@@ -0,0 +1,20 @@
+package com.kjetland.hoursregrest.upgradechecker
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: morten
+ * Date: 27.feb.2010
+ * Time: 22:18:58
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * Class that  tries to get a specific file from github
+ * This file contains a version-string.
+ * if this version-string is larger than
+ * the string pressent in the code, then
+ * the user is alerted that a newer version of this program is pressent for download.
+ * 
+ */
+
+class UpgradeChecker
