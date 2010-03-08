05c5a42f22747eec4e14d418ef575e467b36b7a1
Morten Kjetland <mbk@kjetland.com>
2010-03-08 23:32:11 +0100

    easyer args parsing

--- src/main/scala/com/kjetland/hoursregrest/CmdApp.scala
+++ src/main/scala/com/kjetland/hoursregrest/CmdApp.scala
@@ -19,54 +19,32 @@ import utils.LogHelper
  * To change this template use File | Settings | File Templates.
  */
 
-class ArgException(desc: String, cause : Throwable) extends Exception(desc, cause){
-  def this( desc : String ){
-    this(desc, null)
+class ArgException(desc: String, cause: Throwable) extends Exception(desc, cause) {
+  def this(desc: String) {
+    this (desc, null)
   }
 }
 
-/**
- * convenience class uses to "pop" one settings after another
- */
-class Args(args: Array[String]) {
-  private var index = 0
-
-  def more: Boolean = {
-    return (index < args.length)
-  }
-
-  def reset() = index = 0
-
-  def pop(): String = {
-    if (!more) throw new ArgException("Missing param")
-
-    val arg = args(index)
-    index = index + 1
-    return arg
-  }
-}
-
-object CmdApp extends LogHelper{
 
 
-  private def logEnvironemntInfo{
-    if( logger.isDebugEnabled ){
+object CmdApp extends LogHelper {
+  private def logEnvironemntInfo {
+    if (logger.isDebugEnabled) {
       logger.debug("Environment list:")
-      System.getenv.keySet.toArray.foreach{ x=>
-        logger.debug( "" + x + "=" + System.getenv(x.toString))
+      System.getenv.keySet.toArray.foreach {
+        x =>
+          logger.debug("" + x + "=" + System.getenv(x.toString))
       }
     }
   }
 
   def main(_args: Array[String]) {
 
-    configureLogging( _args)
+    configureLogging(_args)
 
     logEnvironemntInfo
 
     UpgradeChecker.check()
-
-    val args = new Args(_args)
 
     try {
 
@@ -75,12 +53,15 @@ object CmdApp extends LogHelper{
       var username = ""
       var password = ""
 
-      _args.toList match {
+      var argsLeft = _args.toList
+
+      argsLeft match {
         case "-help" :: rest => printHelp; System.exit(0);
-        case "-url" :: _url :: "-username" :: _username :: "-password" :: _password ::rest => {
+        case "-url" :: _url :: "-username" :: _username :: "-password" :: _password :: rest => {
           url = _url
           username = _username
           password = _password
+          argsLeft = rest
         }
         case _ => {
           val settings = new SettingsFile()
@@ -95,126 +76,61 @@ object CmdApp extends LogHelper{
       val client = new ClientImpl(url, browser)
 
       //must find the cmd param
-      var foundCommand = false
-      args.reset()
-      while (args.more) {
-        val arg = args.pop
-        arg match {
-          case "-c" => {
-            val cmdName = args.pop
-            cmdName match {
-              case "lp" => {
-                foundCommand = true
-                val projects = client.projects
-                logger.info("Projects:")
-                projects.foreach {
-                  logger.info(_)
-                }
-              }
-
-              case "lr" => {
-                foundCommand = true
-                val regs = client.registrations
-                logger.info("Registrations:")
-                regs.foreach {
-                  logger.info(_)
-                }
-              }
-
-              case "add" => {
-                foundCommand = true
-
-                var date : DateMidnight = null
-                var project : Project = null
-                var hours : Double= 0
-                var desc : String = null
-
-                //must find all params for this command
-
-                val registration = new Registration
-                while( args.more ){
-                  args.pop() match {
-                    case "-d" => registration.date = DateParser.parseDate( args.pop() )
-                    case "-p" => {
-                      val projectName = args.pop()
-                      registration.project = ProjectResolverObject.resolveProject( projectName, client)
-                    }
-                    case "-h" => registration.hours = HoursParser.parseHours( args.pop())
-                    case "-t" => registration.description = args.pop()
-                  }
-                }
-
-                //verify that we have it all
-                logger.info( "Going to register: " + registration)
-
-                registration.validate
-
-
-                client.selectDate(registration.date)
-                //validate the date
-                if (client.selectedDate.date != registration.date) {
-                  throw new ArgException("Failed setting the date: " + registration.date)
-                }
-
-                //add the registration
-                client.addRegistration(registration.project, registration.hours, registration.description)
-
-                logger.info( "Registration completed." )
-                logger.info("* Now you can go to the website and verify the registrations,\n* then confirm them.")
-
-                }
-
-              case "add-file" => {
-                foundCommand = true
-                //pop the -f param
-                if( args.pop() != "-f" ) throw new ArgException("Missing -f param for command add-file")
-                //pop the filename
-                val filename = args.pop()
-                //parse the file
-                val registrations = RegistrationsFileParser.parse( filename, new ProjectResolverImpl( client ))
-                logger.info( "Starting to add registrations:" )
-                registrations.foreach{ registration =>
-                  
-                  client.selectDate(registration.date)
-                  //validate the date
-                  if (client.selectedDate.date != registration.date) {
-                    throw new ArgException("Failed setting the date: "+registration.date)
-                  }
-
-                  //add the registration
-                  client.addRegistration(registration.project, registration.hours, registration.description)
-                }
-                logger.info( "All "+registrations.size +" registration completed." )
-                logger.info("* Now you can go to the website and verify the registrations,\n* then confirm them.")
-              }
-
-              case _ => throw new ArgException("invalid command: " + arg)
-
-              }
-
-
-            }
-
-            case _ => None
-
+      argsLeft match {
+        case "-c" :: "lp" :: rest => {
+          val projects = client.projects
+          logger.info("Projects:")
+          projects.foreach {
+            logger.info(_)
           }
         }
-  
+        case "-c" :: "lr" :: rest => {
+          val regs = client.registrations
+          logger.info("Registrations:")
+          regs.foreach {
+            logger.info(_)
+          }
+        }
+        case "-c" :: "add" :: "-d" :: _date :: "-p" :: _projectName :: "-h" :: _hours :: "-t" :: _desc :: rest => {
+          val registration = new Registration
+          registration.date = DateParser.parseDate(_date)
+          registration.project = ProjectResolverObject.resolveProject(_projectName, client)
+          registration.hours = HoursParser.parseHours(_hours)
+          registration.description = _desc
 
+          //verify that we have it all
+          logger.info("Going to register: " + registration)
 
-      if (!foundCommand) {
-        logger.info("Error: Did not find command.")
-        printHelp
-        System.exit(-1)
+          client.selectDate(registration.date)
+          //validate the date
+          if (client.selectedDate.date != registration.date) {
+            throw new ArgException("Failed setting the date: " + registration.date)
+          }
+
+          //add the registration
+          client.addRegistration(registration.project, registration.hours, registration.description)
+
+          logger.info("Registration completed.")
+          logger.info("* Now you can go to the website and verify the registrations,\n* then confirm them.")
+
+        }
+
+        case _ => {
+          logger.info("Argument error")
+
+          printHelp
+          System.exit(-1)
+        }
+
       }
 
       System.exit(0)
 
     } catch {
       case e: ArgException => {
-        if( logger.isDebugEnabled) {
+        if (logger.isDebugEnabled) {
           logger.debug("Argument error", e)
-        }else{
+        } else {
           logger.info("Argument error: " + e.getMessage)
         }
         printHelp
@@ -231,8 +147,6 @@ object CmdApp extends LogHelper{
 
 
   }
-
-
 
 
   def printHelp {
@@ -290,11 +204,11 @@ url, username and password:
 
       username = props.getProperty("username")
       if (username.length == 0) throw new Exception("error loading 'username' from " + settingsFilename)
-      if( username == "<specify>") throw new ArgException("You must edit the file " + settingsFilename)
+      if (username == "<specify>") throw new ArgException("You must edit the file " + settingsFilename)
 
       password = props.getProperty("password")
       if (password.length == 0) throw new Exception("error loading 'password' from " + settingsFilename)
-      if( password == "<specify>") throw new ArgException("You must edit the file " + settingsFilename)
+      if (password == "<specify>") throw new ArgException("You must edit the file " + settingsFilename)
 
     }
 
@@ -304,24 +218,25 @@ url, username and password:
    * if one of the args to this program is "-debug", then a different
    * log4j.xml file is used to set up the logging.
    */
-  private def configureLogging(_args: Array[String]){
+  private def configureLogging(_args: Array[String]) {
 
     //check for the -debug arg
     var usingDebug = false
 
-    _args.foreach{ x=>
-      if( x == "-debug"){
-        usingDebug = true
-      }
+    _args.foreach {
+      x =>
+        if (x == "-debug") {
+          usingDebug = true
+        }
     }
 
-    if( !usingDebug){
+    if (!usingDebug) {
       return
     }
 
     val filename = "debug_log4j.xml"
     val url = this.getClass.getClassLoader.getResource(filename)
-    DOMConfigurator.configure( url );
+    DOMConfigurator.configure(url);
     logger.info("Runing program in debug mode. Extra debug info is written to logfile.")
   }
 }
